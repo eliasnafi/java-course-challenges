9532e682639b46660aae5441ccabd87d7f3abfde
Elias Nafi <contact@eliasnafi.com>
2024-08-02 17:22:58 +0200

    Delete Challenge1 - Copy.java

--- Challenge1 - Copy.java
+++
@@ -1,6 +0,0 @@
-public class Challenge1 {
-    public static void main(String[] args) {
-        // 1) Type the main method (remember, you can use the "main" shortcut).
-        // 2) Inside the main method, use System.out.println to print 4 lines of your choice (you can use the "sout" shortcut)
-    }
-}
